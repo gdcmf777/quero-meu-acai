a5f6b2b0a173908f2d064f509b8f500f192c4818
rafaelfern <gdcmf777@gmail.com>
2023-03-15 08:16:21 -0300

    Adding input customer field in first step

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,41 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useRef } from 'react'
 import Link from 'next/link'
 import styles from '../styles/styles.module.scss'
 import Button from '@/components/Button'
-import { mock } from '../mock/mock'
+import api from '../utils/api'
 import { IFlavorMock } from '@/types'
 import Cards from '@/components/Cards'
 import { CustomerRequestContext } from '@/context/CustomerRequestContext'
+import { FaArrowCircleRight } from "react-icons/fa" 
 
-export default function Home() {
+
+
+export default function Home({ data }: object ) {
   
+  const inputUserRef = useRef(null);
   const [flavorKind, setFlavorKind] = useState<IFlavorMock[]>()
+  const [slideInputContent, setSlideInputContent] = useState(false);
   const { reqFlavor, reqSize } = useContext(CustomerRequestContext)
 
-  const loadMock = () => setFlavorKind(mock)
+  const loadProducts = async () => {
+    setFlavorKind(data)
+    setSlideInputContent(true)
+  }
 
   return (
     <>
       <main className={styles.contentContainer}>
-        <div className={styles.sloganHome}>
-          <h2>Açaí: o sabor do Brasil em cada tigela!</h2>
-        </div>
-        <Button titleBtn="Monte seu pedido" loadMock={loadMock}/>
+        
+        <section className={`${styles.nameContainer} ${slideInputContent ? styles.slideOutClass : '' }`}>
+          <h4>Para começarmos</h4>
+          <h1>Informe seu nome</h1>
+          <div className={styles.inputContent}>
+            <input ref={inputUserRef}/>
+            <FaArrowCircleRight size={30} onClick={() => loadProducts()}/>
+          </div>
+        </section>
+
+        {/* <Button titleBtn="Continuar" loadMock={loadProducts}/> */}
         {flavorKind&&
             <div className={styles.flavorContent}>
                 <h2>Selecione um sabor:</h2>
@@ -48,4 +63,17 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+
+  let data = [{}]
+  const response = await api.get('/produtos');
+  data = response.data
+
+  return {
+    props: {
+      data,
+    }
+  }
 }
---
+++ src/utils/variables.ts
@@ -0,0 +1,5 @@
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export {
+  apiUrl
+};
